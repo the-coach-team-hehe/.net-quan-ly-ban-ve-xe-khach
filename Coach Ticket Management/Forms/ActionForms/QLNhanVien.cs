3290ebf556a56d299afef016a1ff55b1a82d0174
kinezlogin <106385769+kinezlogin@users.noreply.github.com>
2023-11-23 21:07:33 +0700

    add form QL NhanVien

---
+++ Coach Ticket Management/Forms/ActionForms/QLNhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coach_Ticket_Management.Forms.ActionForms
+{
+    public partial class QLNhanVien : Form
+    {
+        public QLNhanVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
