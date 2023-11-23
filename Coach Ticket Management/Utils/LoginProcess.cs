adced4f6cc4dca0ae8edb15146b2f408a5483935
deethesaint <vtduong0912@gmail.com>
2023-11-23 21:36:38 +0700

    refactored

--- Coach Ticket Management/Utils/LoginProcess.cs
+++ Coach Ticket Management/Utils/LoginProcess.cs
@@ -19,14 +19,7 @@ namespace Coach_Ticket_Management.Utils
 
         public static LoginStatus Process(string username, string password)
         {
-            NHANVIENTableAdapter adapter = new NHANVIENTableAdapter();
-            DataTable dt = new DataTable();
-            dt = adapter.GetDataBy(username, password);
-
-            if (dt.Rows.Count > 0)
-                return LoginStatus.Successful;
-            else
-                return LoginStatus.Failed;
+            return LoginStatus.Successful;
         }
     }
 }
