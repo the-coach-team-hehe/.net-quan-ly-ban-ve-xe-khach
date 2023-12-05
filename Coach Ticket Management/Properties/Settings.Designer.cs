d419e383e36732a474e0b010d9be9084f25b72f0
deethesaint <vtduong0912@gmail.com>
2023-12-06 00:17:16 +0700

    connectionString (settings) changed

--- Coach Ticket Management/Properties/Settings.Designer.cs
+++ Coach Ticket Management/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace Coach_Ticket_Management.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|DB_QLBVXK_Prim" +
-            ".mdf;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\DB_QLBVXK_Pri" +
+            "m.mdf;Integrated Security=True;Connect Timeout=30")]
         public string DB_QLBVXK_PrimConnectionString {
             get {
                 return ((string)(this["DB_QLBVXK_PrimConnectionString"]));
